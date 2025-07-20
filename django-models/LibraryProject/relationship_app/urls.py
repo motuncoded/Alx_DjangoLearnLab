3d489ea6c7852af520454bcdc59d0784d938c90b
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-07-21 00:41:33 +0100

    task 3

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,20 +1,22 @@
 from django.urls import path
-from .views import list_books
+from .views import list_books, add_book, edit_book, delete_book, LibraryDetailView
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 
-path('roles/', include('relationship_app.urls')),
 urlpatterns = [
     path('books/', list_books, name='list-books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
     path('register/', views.register, name='register'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
- path('admin/', views.admin_view, name='admin-view'),
+    path('admin/', views.admin_view, name='admin-view'),
     path('librarian/', views.librarian_view, name='librarian-view'),
     path('member/', views.member_view, name='member-view'),
-      # ... other URL patterns ...
-    path('books/add/', views.add_book, name='add-book'),
-    path('books/<int:pk>/edit/', views.edit_book, name='edit-book'),
-    path('books/<int:pk>/delete/', views.delete_book, name='delete-book'),
+    # ... other URL patterns ...
+
+    # Add, edit, and delete book URLs
+    path('books/add/', add_book, name='add-book'),
+    path('books/<int:pk>/edit/', edit_book, name='edit-book'),
+    path('books/<int:pk>/delete/', delete_book, name='delete-book'),
 ]
