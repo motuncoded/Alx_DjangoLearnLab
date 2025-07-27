5f7df221669c9092b70596446967b3207a7355b4
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-07-27 12:42:32 +0100

    task 3

--- advanced_features_and_security/bookshelf/admin.py
+++ advanced_features_and_security/bookshelf/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+
+from advanced_features_and_security.LibraryProject.relationship_app.models import CustomUser
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
@@ -10,6 +12,22 @@ class BookAdmin(admin.ModelAdmin):
     
     # Add filters for publication_year
     list_filter = ('publication_year',)
-
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_superuser')
+    list_filter = ('is_staff', 'is_superuser')
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_superuser'),
+        }),
+    )
 # Register the Book model with the custom admin class
 admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
