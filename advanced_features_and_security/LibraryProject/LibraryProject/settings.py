aabc2992d61ac28b8350f119b78d0c1ac6574b98
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-07-27 13:45:32 +0100

    task 3

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -1,2 +1,12 @@
 # settings.py
 AUTH_USER_MODEL = 'bookshelf.CustomUser'
+
+
+DEBUG = True
+
+# Security settings
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = "DENY"
+SECURE_CONTENT_TYPE_NOSNIFF = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
