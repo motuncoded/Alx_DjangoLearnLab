aa0bca1a6ec5cc2de9058685944d1587d64cb4e4
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-07-27 12:56:43 +0100

    task 3

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -7,3 +7,20 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
+    
+
+
+class CustomUser(AbstractUser):
+    username = None  # Remove the username field
+    email = models.EmailField(unique=True)
+
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []  # You can add additional required fields here
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email  
